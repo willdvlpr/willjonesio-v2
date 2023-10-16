98f4f85942057f354944fc834a0b95b716f53299
Will Jones <willjones484@gmail.com>
2023-10-16 17:38:35 +1100

    remove some boiler plate and add nav

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,17 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
+  root: false,
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'vue/no-multiple-template-root': 'off'
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: '2021',
+    sourceType: 'module'
   }
 }
---
+++ src/components/NavLink.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps(['label', 'underline']);
+
+const linkStyle = computed(() => ({
+  textDecoration: props.underline ? 'underline' : 'none'
+}));
+</script>
+
+<template>
+  <button :style="linkStyle">
+    {{ label }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  border: none;
+  background: none;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  cursor: pointer;
+  color: #000;
+}
+</style>
